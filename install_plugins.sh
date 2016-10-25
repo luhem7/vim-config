a10f0a1d98ffae45e054f257d36bc115fb678416
Mehul Gangavelli <mehul.gang@gmail.com>
2016-10-25 13:56:34 -0400

    Added support for Pathogen and CtrlP plugins

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VIM_DIR=$HOME/vimfiles/
-if [ "$1" = "-l" ]; then
+if [ "$1" == "-l" ]; then
     echo "Using linux vim files directory"
     VIM_DIR=$HOME/.vim/
 fi
@@ -21,6 +21,28 @@ function create_dir_if_absent {
     fi
 }
 
+function check_file_exists {
+    echo "Checking to see if "$1"file is present."
+    if [ ! -f $1 ]; then
+        echo "File was missing"
+        return 1
+    else
+        echo "File present"
+        return 0
+    fi
+}
+
+function check_dir_exists {
+    echo "Checking to see if "$1"directory is present."
+    if [ ! -d $1 ]; then
+        echo "Directory was missing"
+        return 1
+    else
+        echo "Directory present"
+        return 0
+    fi
+}
+
 function check_pathogen {
     PATHOGEN_DL="https://tpo.pe/pathogen.vim"
     echo "Checking to see if pathogen vim file is present."
@@ -34,6 +56,21 @@ function check_pathogen {
    create_dir_if_absent $BUNDLE_DIR
 }
 
+function check_ctrlp {
+    CTRLP_DIR=$BUNDLE_DIR"ctrlp.vim/"
+    CTRLP_GIT="https://github.com/ctrlpvim/ctrlp.vim.git"
+
+    echo "CtrlP Plugin: "
+
+    check_dir_exists $CTRLP_PATH
+    if [ "$?" -eq 1 ]; then
+        echo "Downloading CtrlP Plugin"
+        git clone $CTRLP_GIT $CTRLP_PATH
+    else
+        echo "CtrlP present"
+    fi
+}
+
 echo $LINE
 create_dir_if_absent $VIM_DIR
 echo $LINE
@@ -42,5 +79,7 @@ echo $LINE
 
 #Checking if pathogen vim plugin is present
 check_pathogen
+echo $LINE
+check_ctrlp
 echo $LINE
 
